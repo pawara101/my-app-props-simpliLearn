7e6656c51f4d6caa30f4a8ef3f5f2827ac428ce5
pawara101 <pawara101dassanayake@gmail.com>
2024-01-07 21:53:08 +0530

    SimpliLearn tutorial day 01| props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './Classprops.js';
+import Classprops from './Classprops.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Classprops name="Leraner 1"/>
+      <Classprops name="James"/>
     </div>
   );
 }
