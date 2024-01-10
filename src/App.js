7386c9692d649f05e79997026ea3293fe5316912
pawara101 <pawara101dassanayake@gmail.com>
2024-01-10 20:10:29 +0530

    SimpliLearn tutorial day 02| Components | V1.0

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import "./App.css";
 import React from "react";
 import NewComp from "./Components/NewComp";
 
-class App extends React.Component {
-  styles = {
-    fontStyle: "bold",
-    color: "teal"
-  };
-  render() {
-    return (
-    <div className="App">
-      <h1 style={this.styles}>Welcome</h1>
-      <NewComp></NewComp>
-    </div>
-    );
-  }
+import FunctionalComp from "./Components/FunctionalComp";
+import ClassComponent from "./Components/ClassComp";
+
+function App() {
+  return <div>
+    <h1>Hello tutorial</h1>
+    <h1>Hello tutorial for Component</h1> 
+    <FunctionalComp></FunctionalComp>
+    <ClassComponent></ClassComponent>
+  
+  </div>
+
+  
+  
 }
 
 export default App;
---
+++ src/Components/ClassComp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+
+    render() {
+        return (<div>
+            <p>This is a Class Component...</p>
+        </div>);
+    }
+}
+
+export default ClassComponent;
